f352da3d697bb3b719e9826ceb6c993df4c9c443
Niklas Oemler <niklas.oemler@icloud.com>
2023-02-10 10:36:34 +0100

    Initial Commit

---
+++ EurowingsChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EurowingsChallenge
+//
+//  Created by Niklas Oemler on 10.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
