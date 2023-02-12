1497ca8f4d58ecbb1ce15e6ffec5109bd6a3a325
Niklas Oemler <niklas.oemler@icloud.com>
2023-02-12 13:37:47 +0100

    ♻️  reset tableview background before setting state

--- EurowingsChallenge/Pages/Post Detail/PostDetailViewState.swift
+++ EurowingsChallenge/Pages/Post Detail/PostDetailViewState.swift
@@ -10,20 +10,20 @@ import Foundation
 protocol PostDetailViewState: ViewState {
     var post: Post { get }
     var author: User { get }
-    var comments: [Comment] { get }
+    var comments: [Comment]? { get }
     var showComments: Bool { get set }
 }
 
 struct DefaultPostDetailViewState: PostDetailViewState {
     var post: Post
     var author: User
-    var comments: [Comment]
+    var comments: [Comment]?
     var showComments: Bool = false
 }
 
 struct MockPostDetailViewState: PostDetailViewState {
     var post: Post = MockProvider.post
     var author: User = MockProvider.user
-    var comments: [Comment] = MockProvider.comments
+    var comments: [Comment]? = MockProvider.comments
     var showComments: Bool = false
 }
