871f241683cd1b965df46163867e0824a2155dd3
Niklas Oemler <niklas.oemler@icloud.com>
2023-02-12 13:36:22 +0100

    ♻️  refactor recceiving of failure completion in viewmodels

--- EurowingsChallenge/Core/ViewStates/ErrorViewState.swift
+++ EurowingsChallenge/Core/ViewStates/ErrorViewState.swift
@@ -10,11 +10,13 @@ import Foundation
 protocol ErrorViewState: ViewState {
     var title: String { get }
     var description: String { get }
+    var error: Error? { get }
     var buttonTitle: String { get }
 }
 
 struct DefaultErrorViewState: ErrorViewState {
     var title = "Error"
     var description = "An Error occured fetching the data"
+    var error: Error? = nil
     var buttonTitle: String = "Try again"
 }
